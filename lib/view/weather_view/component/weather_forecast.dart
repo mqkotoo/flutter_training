a4729767c6c450fc556cef16bf15b8473ce4a9f8
mqkotoo <m_murkami@yumemi.co.jp>
2023-08-04 10:36:12 +0900

    Create weather_forecast.dart as a component

---
+++ lib/view/weather_view/component/weather_forecast.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class WeatherForecast extends StatelessWidget {
+  const WeatherForecast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //Placeholder
+        const AspectRatio(
+          aspectRatio: 1,
+          child: Placeholder(),
+        ),
+        const SizedBox(height: 16),
+        //Temperature
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                '** ℃',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                '** ℃',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+}
--- lib/view/weather_view/weather_page.dart
+++ lib/view/weather_view/weather_page.dart
@@ -1,49 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/view/weather_view/component/weather_forecast.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: FractionallySizedBox(
           widthFactor: 0.5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //Placeholder
-              const AspectRatio(
-                aspectRatio: 1,
-                child: Placeholder(),
-              ),
-              const SizedBox(height: 16),
-              //Temperature
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      '** ℃',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      '** ℃',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(),
-            ],
-          ),
+          child: WeatherForecast(),
         ),
       ),
     );
