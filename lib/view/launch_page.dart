6bd6f4de7723ddd0a6d7610c73c11cae9d2d8a4f
mqkotoo <m_murkami@yumemi.co.jp>
2023-08-09 13:39:16 +0900

    Implement Navigation to WeatherPage

--- lib/view/launch_page.dart
+++ lib/view/launch_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/repository/weather_repository.dart';
+import 'package:flutter_training/view/weather_view/weather_page.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 class LaunchPage extends StatefulWidget {
   const LaunchPage({super.key});
@@ -8,6 +11,27 @@ class LaunchPage extends StatefulWidget {
 }
 
 class _LaunchPageState extends State<LaunchPage> {
+  Future<void> _toWeatherPage() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    if (!mounted) {
+      return;
+    }
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) => WeatherPage(
+          weather: WeatherRepository(client: YumemiWeather()),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.endOfFrame.then((_) => _toWeatherPage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.green);
