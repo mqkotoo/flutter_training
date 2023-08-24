7f5834ddcd84e66c4d4801b2771b94ecac02a328
mqkotoo <m_murkami@yumemi.co.jp>
2023-08-24 12:35:05 +0900

    Create AfterDisplay mixin

--- lib/view/launch_page.dart
+++ lib/view/launch_page.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/view/weather_view/weather_page.dart';
 
+mixin AfterDisplay<T extends StatefulWidget> on State<T> {
+  void afterDisplay();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.endOfFrame.then((_) {
+      afterDisplay();
+    });
+  }
+}
+
 class LaunchPage extends StatefulWidget {
   const LaunchPage({super.key});
 
