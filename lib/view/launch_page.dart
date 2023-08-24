60c6c62c87b044bdf6c7233ae73938417e0aaeda
mqkotoo <m_murkami@yumemi.co.jp>
2023-08-24 12:41:20 +0900

    Change Mixin naming

--- lib/view/launch_page.dart
+++ lib/view/launch_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/view/weather_view/weather_page.dart';
 
-mixin AfterDisplay<T extends StatefulWidget> on State<T> {
+mixin AfterDisplayMixin<T extends StatefulWidget> on State<T> {
   void afterDisplay();
 
   @override
@@ -20,7 +20,7 @@ class LaunchPage extends StatefulWidget {
   State<LaunchPage> createState() => _LaunchPageState();
 }
 
-class _LaunchPageState extends State<LaunchPage> with AfterDisplay {
+class _LaunchPageState extends State<LaunchPage> with AfterDisplayMixin {
   Future<void> _toWeatherPage() async {
     await Future<void>.delayed(const Duration(milliseconds: 500));
     if (!mounted) {
