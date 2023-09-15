cb4d3858039fc0655d8e7be6a43ee5c82dfd942b
mqkotoo <m_murkami@yumemi.co.jp>
2023-09-15 18:48:25 +0900

    Create UiState

---
+++ lib/state/ui_state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_training/model/weather_data.dart';
+import 'package:flutter_training/model/weather_request.dart';
+import 'package:flutter_training/service/weather_service.dart';
+import 'package:flutter_training/utils/api/result.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'ui_state.g.dart';
+
+@riverpod
+class UiState extends _$UiState {
+  @override
+  WeatherData? build() => null;
+
+  void getWeather({
+    required WeatherRequest request,
+    required void Function(String error) onError,
+  }) {
+    return switch (ref.read(weatherServiceProvider).fetchWeather(request)) {
+      Success(value: final value) => state = value,
+      Failure(exception: final error) => onError.call(error),
+    };
+  }
+}
