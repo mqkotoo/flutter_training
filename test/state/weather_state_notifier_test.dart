0051ddfbfcd8c5d5a71c61802241cd2ed7ca544a
mqkotoo <m_murkami@yumemi.co.jp>
2023-09-28 17:46:49 +0900

    Add WeatherStateNotifier success case

---
+++ test/state/weather_state_notifier_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/model/weather_condition.dart';
+import 'package:flutter_training/model/weather_forecast.dart';
+import 'package:flutter_training/model/weather_request.dart';
+import 'package:flutter_training/service/weather_service.dart';
+import 'package:flutter_training/state/weather_state_notifier.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+import 'weather_state_notifier_test.mocks.dart';
+
+const jsonData = '''
+        {
+          "weather_condition": "cloudy",
+          "max_temperature": 25, 
+          "min_temperature": 7,
+          "date": "2023-09-19T00:00:00.000"
+        }
+        ''';
+
+final request = WeatherRequest(
+  area: 'Nagoya',
+  date: DateTime(2023, 9, 19),
+);
+
+@GenerateNiceMocks([MockSpec<YumemiWeather>()])
+void main() {
+  final mockClient = MockYumemiWeather();
+  late ProviderContainer container;
+
+  setUpAll(() {
+    //mockのYumemiWeatherでDIする
+    container = ProviderContainer(
+      overrides: [yumemiWeatherClientProvider.overrideWithValue(mockClient)],
+    );
+  });
+
+  tearDownAll(() {
+    container.dispose();
+  });
+
+  test('success case: getWeather()', () {
+    when(mockClient.fetchWeather(any)).thenReturn(jsonData);
+
+    //　天気の取得処理を実行して、結果をstateに流す
+    container
+        .read(weatherStateNotifierProvider.notifier)
+        .getWeather(request: request, onError: (error) {});
+
+    final weatherState = container.read(weatherStateNotifierProvider);
+
+    expect(weatherState, isNotNull);
+
+    expect(
+      weatherState,
+      WeatherForecast(
+        weatherCondition: WeatherCondition.cloudy,
+        maxTemperature: 25,
+        minTemperature: 7,
+        date: DateTime(2023, 9, 19),
+      ),
+    );
+  });
+}
